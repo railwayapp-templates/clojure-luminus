adc47f0ba460056dc919dbad30f47b6859e3b84f
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-11-06 16:20:06 +0000

    Use normal nixpacks

--- start-app.sh
+++
@@ -1,8 +0,0 @@
-# start-app.sh
-#!/bin/bash
-
-# Run migrations
-lein run migrate
-
-# Find and run the built JAR file
-java -jar $(find ./target -name '*.jar' ! -name '*SNAPSHOT*') migrate && java -jar $(find ./target -name '*.jar' ! -name '*SNAPSHOT*')
