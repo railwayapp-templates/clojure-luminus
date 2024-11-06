7cdb002f9ac6680724687b66100a2e89fee0f7d0
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-11-06 16:12:59 +0000

    try another approach

--- start-app.sh
+++ start-app.sh
@@ -5,4 +5,4 @@
 lein run migrate
 
 # Find and run the built JAR file
-java -jar $(find ./target -name '*.jar' ! -name '*SNAPSHOT*')
+java -jar $(find ./target -name '*.jar' ! -name '*SNAPSHOT*') migrate && java -jar $(find ./target -name '*.jar' ! -name '*SNAPSHOT*')
